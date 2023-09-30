035162e6c3ee53d68e3a755dfdf751010c8d3e03
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-09-30 16:48:20 -0700

    Add css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
 
 // Import MUI default Roboto font
 import '@fontsource/roboto/300.css';
@@ -11,6 +12,7 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline enableColorScheme />
     <App />
   </React.StrictMode>,
 )
