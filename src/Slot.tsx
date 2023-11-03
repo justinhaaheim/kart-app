c7c813b59eda85a18e764045e5d344fde1468808
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-02 23:27:41 -0500

    Get sizing and overflow looking right

--- src/Slot.tsx
+++ src/Slot.tsx
@@ -12,8 +12,13 @@ type Props = {
   selectedIndex: number | null;
 };
 
+function getPx(value: number): string {
+  return `${value}px`;
+}
+
 const SLOT_SIZE_PX = 140;
-const SLOT_CONTAINER_SIZE_PX = SLOT_SIZE_PX + 20;
+const SLOT_MARGIN_PX = 10;
+const SLOT_CONTAINER_SIZE_PX = SLOT_SIZE_PX + SLOT_MARGIN_PX * 2;
 
 const lightTheme = getTheme({mode: 'light'});
 console.log('lightTheme', lightTheme);
@@ -24,33 +29,32 @@ export default function Slot({label, options, selectedIndex}: Props) {
       <Stack spacing={0.5}>
         <Paper
           sx={{
-            alignItems: 'center',
-            display: 'flex',
-            height: SLOT_CONTAINER_SIZE_PX,
-            justifyContent: 'center',
-            width: SLOT_CONTAINER_SIZE_PX,
+            display: 'block',
+            height: getPx(SLOT_CONTAINER_SIZE_PX),
+            overflow: 'hidden',
+            padding: getPx(SLOT_MARGIN_PX),
+            width: getPx(SLOT_CONTAINER_SIZE_PX),
           }}
           variant="outlined">
-          <Box
-            sx={{
-              alignItems: 'center',
-              border: '1px solid red',
-              display: 'flex',
-              height: SLOT_SIZE_PX,
-              justifyContent: 'center',
-              padding: 2,
-              textAlign: 'center',
-              verticalAlign: 'middle',
-              width: SLOT_SIZE_PX,
-            }}>
-            <Typography component="div" variant="body1">
-              {
-                options[
-                  selectedIndex ?? Math.floor(Math.random() * options.length)
-                ]
-              }
-            </Typography>
-          </Box>
+          {options.map((option, index) => (
+            <Box
+              key={index}
+              sx={{
+                alignItems: 'center',
+                border: '1px solid red',
+                display: 'flex',
+                height: getPx(SLOT_SIZE_PX),
+                justifyContent: 'center',
+                // padding: 2,
+                textAlign: 'center',
+                verticalAlign: 'middle',
+                width: getPx(SLOT_SIZE_PX),
+              }}>
+              <Typography component="div" variant="body1">
+                {option}
+              </Typography>
+            </Box>
+          ))}
         </Paper>
 
         <Typography variant="subtitle2">{label}</Typography>
