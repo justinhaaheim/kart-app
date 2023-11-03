80b2efbfb23daf47ad50dfbad303c0471da336b7
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-02 20:32:43 -0500

    More WIP add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 // import './App.css';
 
+import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 // import Grid from '@mui/material/Unstable_Grid2';
 // import {useState} from 'react';
 
 function App() {
-  return <Paper sx={{margin: '0 auto', padding: 3}}>Some stuff</Paper>;
+  return (
+    <Container
+      component="main"
+      maxWidth="md"
+      sx={{
+        mb: 5,
+        mt: 2,
+      }}>
+      <Paper sx={{margin: '0 auto', padding: 3}}>Some stuff</Paper>
+    </Container>
+  );
 }
 
 export default App;
