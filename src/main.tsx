6e2c5193613ad9fa4d5916069dc9e28885c7ce97
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-02 20:28:40 -0500

    WIP start clearing out vite stuff

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,18 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider} from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline enableColorScheme />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/theme.ts
@@ -0,0 +1,29 @@
+import {createTheme} from '@mui/material/styles';
+
+// function getHtmlFontSize(): number | null {
+//   let fontSize: number | null = null;
+//   try {
+//     // NOTE: Even when this is not explicitly set it still returns a value
+//     const fontSizeString = window
+//       .getComputedStyle(document.getElementsByTagName('html')[0])
+//       .getPropertyValue('font-size');
+//     fontSize = parseInt(fontSizeString, 10);
+//   } catch (e) {
+//     console.error('Error getting font size', e);
+//   }
+
+//   return fontSize;
+// }
+
+// const htmlFontSize = getHtmlFontSize();
+
+// const htmlFontSizeObject =
+//   htmlFontSize == null ? {} : {htmlFontSize: htmlFontSize};
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default theme;
