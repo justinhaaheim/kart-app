fec7e4b2e7333409fe4cb79c9d75d175cf5b9042
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-07 02:35:39 -0600

    Finish

--- src/soundPlayer.ts
+++ src/soundPlayer.ts
@@ -18,6 +18,13 @@ const SLOWER_SECTION_DURATION_MS = 2600;
 
 const rouletteSoundUrls = [r1, r2, r3, r4, r5, r6, r7, r8];
 
+// I should probably do this after the page loads, but "I'm lazy", according to github copilot
+const audioContext = new AudioContext();
+const rouletteSoundsBuffers = await Promise.all(
+  rouletteSoundUrls.map(async (url) => getAudioBuffer(audioContext, url)),
+);
+const rouletteEndSoundBuffer = await getAudioBuffer(audioContext, rDecide);
+
 async function getAudioBuffer(
   audioContext: AudioContext,
   filepath: string,
@@ -28,79 +35,79 @@ async function getAudioBuffer(
   return audioBuffer;
 }
 
+function scheduleBufferAtTime({
+  timeFromNowMs,
+  bufferNode,
+  audioContext,
+}: {
+  audioContext: AudioContext;
+  bufferNode: AudioBufferSourceNode;
+  timeFromNowMs: number;
+}) {
+  const scheduledTime = audioContext.currentTime + timeFromNowMs / 1000;
+  // console.log(
+  //   `Scheduling sound ${timeFromNowMs}ms from now at ${scheduledTime}`,
+  // );
+  bufferNode.connect(audioContext.destination);
+  bufferNode.start(timeFromNowMs === 0 ? undefined : scheduledTime);
+}
+
 async function playSoundsWithDelay(
   delayMs: number,
   duration: number,
+  offsetMs: number,
   audioContext: AudioContext,
   counterRef: {current: number},
 ) {
-  const rouletteSounds = await Promise.all(
-    rouletteSoundUrls.map(async (url) => getAudioBuffer(audioContext, url)),
-  );
-
-  // const rouletteSoundsBufferNodes = rouletteSounds.map(
-  //   (buffer) => new AudioBufferSourceNode(audioContext, {buffer: buffer}),
-  // );
-
-  // rouletteSoundsBufferNodes.forEach((node) =>
-  //   node.connect(audioContext.destination),
-  // );
-
   const soundsToPlayCount = Math.floor(duration / delayMs);
 
   Array.from({length: soundsToPlayCount}).forEach((_, i) => {
-    const scheduledTime = audioContext.currentTime + (i * delayMs) / 1000;
-    // console.log(`scheduling sound ${i} for time ${scheduledTime}`);
-    const bufferNode = new AudioBufferSourceNode(audioContext, {
-      buffer: rouletteSounds[counterRef.current % rouletteSounds.length],
-    });
-    counterRef.current += 1;
-    bufferNode.connect(audioContext.destination);
-    bufferNode.start(scheduledTime);
-  });
+    const timeFromNowMs = offsetMs + i * delayMs;
 
-  // return interval;
+    const bufferNode = new AudioBufferSourceNode(audioContext, {
+      buffer:
+        rouletteSoundsBuffers[
+          counterRef.current % rouletteSoundsBuffers.length
+        ],
+    });
+    scheduleBufferAtTime({audioContext, bufferNode, timeFromNowMs});
+    counterRef.current += 1;
+  });
 }
 
 export async function playRouletteSound(durationSeconds: number) {
-  const audioContext = new AudioContext();
-  // const audio = new Audio('/roulette.mp3');
-
   const durationMs = durationSeconds * 1000;
 
   const counterRef = {current: 0};
 
-  const quickSoundsDuration = durationMs - SLOWER_SECTION_DURATION_MS;
+  const fastSoundsDuration =
+    durationMs - SLOWER_SECTION_DURATION_MS - FINAL_DELAY_MS;
 
   await playSoundsWithDelay(
     SHORT_DELAY_MS,
-    quickSoundsDuration,
+    fastSoundsDuration,
+    0,
     audioContext,
     counterRef,
   );
-  // const interval = setInterval(() => {
-  //   const a = new Audio(rouletteSounds[counter % rouletteSounds.length]);
-  //   a.play();
-  //   counter += 1;
-  // }, SHORT_DELAY_MS);
 
-  // setTimeout(
-  //   () => {
-  //     console.log('switching to slower');
-  //     clearInterval(interval);
-  //     interval = playSoundsWithDelay(LONG_DELAY_MS, counterRef);
-  //   },
-  //   durationMs - LONG_DELAY_MS - SLOWER_SECTION_DURATION_MS,
-  // );
+  await playSoundsWithDelay(
+    LONG_DELAY_MS,
+    SLOWER_SECTION_DURATION_MS,
+    fastSoundsDuration,
+    audioContext,
+    counterRef,
+  );
 
-  // setTimeout(() => {
-  //   clearInterval(interval);
-  //   setTimeout(() => {
-  //     const a = new Audio(rDecide);
-  //     a.play();
-  //   }, FINAL_DELAY_MS);
-  // }, durationMs - FINAL_DELAY_MS);
+  const rouletteEndSoundBufferNode = new AudioBufferSourceNode(audioContext, {
+    buffer: rouletteEndSoundBuffer,
+  });
 
-  // audio.volume = 0.25;
-  // audio.play();
+  // The final sound should be played right *on* the durationSeconds provided, not before
+
+  scheduleBufferAtTime({
+    audioContext,
+    bufferNode: rouletteEndSoundBufferNode,
+    timeFromNowMs: durationMs,
+  });
 }
