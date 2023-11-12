ebc9f2ca8d67bcc2a4cf97d64a3633b314352897
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-12 01:30:16 -0800

    Try disabling zoom

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@
 import Picker from './Picker';
 // import Grid from '@mui/material/Unstable_Grid2';
 // import {useState} from 'react';
+// import useDisableZoom from './useDisableZoom';
 
 function App() {
+  // useDisableZoom(true);
+
   return <Picker />;
 }
 
